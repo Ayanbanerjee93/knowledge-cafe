61ee4b20cd58c25dbfb259d09f4c8486da135faf
Ayanbanerjee93 <ayanbanerjee.1893@gmail.com>
2023-12-30 13:44:18 +0530

    header added

---
+++ src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Blogs.css';
+
+const Blogs = () => {
+    return (
+        <div className='blog-container'>
+
+            {/* all blogs display area */}
+
+           <div className="blog-body">
+
+                 <h1>this is a blog area</h1>
+            
+           </div>
+
+
+           {/* bookmark section */}
+        <div className="bookmark-area">
+            <h3>All book marks showing here</h3>
+            </div>
+        </div>
+
+    );
+};
+
+export default Blogs;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Header.css';
+import profile_image from '../../../images/profile.png'
+
+const Header = () => {
+    return (
+        <nav>
+              <div className="header">
+
+            <div>
+                <h1>Knowledge Cafe</h1>
+            </div>
+
+            <div className="nav-container">
+                   {/* nav menu div */}
+            <div className="nav-menu">
+                <a href="/home">Home</a>
+                <a href="/about">About</a>
+                <a href="/visit">Visit Us</a>
+                <a href="/login">Login</a>
+
+             </div>
+                    
+                    {/* image div */}
+                <img src={profile_image} srcset="" />
+               
+            </div>
+            </div>
+
+                <hr />        
+             
+        </nav>
+        
+            
+    );
+};
+
+export default Header;
